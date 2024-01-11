b6a0000aa09d4d1ffff643392590deec277fd2b8
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-01-11 19:08:46 +0530

    hangman game, login page and todolist

--- TS/hangman-game/src/App.tsx
+++ TS/hangman-game/src/App.tsx
@@ -1,51 +1,100 @@
-import { useState, useEffect } from 'react';
-import wordsData from './wordList.json';
-import HangmanDrawing from './HangmanDrawing';
-import HangmanWord from './HangmanWord';
-import Keyboard from './Keyboard';
+import { useCallback, useEffect, useState } from "react"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
+import words from "./wordList.json"
 
-interface WordData {
-  words: string[];
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
 }
 
-const words: WordData = wordsData;
- 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    const randomIndex = Math.floor(Math.random() * words.words.length);
-    return words.words[randomIndex];
-  });
+  const [wordToGuess, setWordToGuess] = useState(getWord())
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  const[guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+  )
+
+  const isLoser = incorrectLetters.length >= 6
+  const isWinner = Array.from(wordToGuess).every(letter => guessedLetters.includes(letter))
+
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    },
+    [guessedLetters, isWinner, isLoser]
+  )
 
   useEffect(() => {
-    console.log(wordToGuess); 
-  }, []); 
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (key !== "Enter") return
+
+      e.preventDefault()
+      setGuessedLetters([])
+      setWordToGuess(getWord())
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
 
   return (
-    <>
-      <p>{wordToGuess}</p>
-      <p>Hello ts project</p>
-      <div 
+    <div
       style={{
-        maxWidth:"800px",
+        maxWidth: "800px",
         display: "flex",
         flexDirection: "column",
-        gap :"2rem",
-        margin:"0 auto",
-        alignItems:"center"
-
-      }}>
-        <div style= {{ fontSize:'2rem', textAlign:"center"}}>
-          Lose 
-          Win
-        </div>
-        <HangmanDrawing/>
-        <HangmanWord />
-        <Keyboard />
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {isWinner && "Winner! - Refresh to try again"}
+        {isLoser && "Nice Try - Refresh to try again"}
       </div>
-    </>
-  );
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter(letter =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ TS/login_page/src/App.tsx
@@ -0,0 +1,41 @@
+
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="login-box">
+      <div className="login-header">
+        <header>Login</header>
+      </div>
+      <div className="input-box">
+        <input type="text" className="input-field" placeholder='Email' autoComplete='off' required></input>
+      </div>
+      <div className="input-box">
+        <input type="password" className="input-field" placeholder='Password' autoComplete='off' required></input>
+      </div>
+      <div className="forgot">
+        <section>
+          <input type="checkbox" id = "check"></input>
+          <label htmlFor="check">Remember me</label>
+        </section>
+        <section>
+          <a href="#">Forgot password</a>
+        </section>
+      </div>
+      <div className="input-submit">
+      <button class="submit-btn" id="submit"></button>
+            <label for="submit">Sign In</label>
+      </div>
+      <div className="sign-up-link">
+        <p>Don't have an account yet? <Link to ="#">Sign Up</Link></p>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
