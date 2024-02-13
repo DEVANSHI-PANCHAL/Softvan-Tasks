4013c2c472187e5300cfa02137c778a91efb7ec7
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-02-13 22:36:52 +0530

    header footer added

--- Blog/client/src/App.jsx
+++ Blog/client/src/App.jsx
@@ -7,11 +7,13 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <BrowserRouter>
-   
+   <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
@@ -20,7 +22,7 @@ function App() {
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
     </Routes>
-      
+      <Footer/>
     </BrowserRouter>
   )
 }
