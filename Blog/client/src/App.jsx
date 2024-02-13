5d56dcc0f84020a57d19e98d7d1806b392e0a171
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-02-13 21:38:12 +0530

    blog-app-added routes

---
+++ Blog/client/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+function App() {
+
+  return (
+    <BrowserRouter>
+   
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+    </Routes>
+      
+    </BrowserRouter>
+  )
+}
+
+export default App
