35412ca3ac0d7a1d9a14603cfe564519ee7b3daf
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-03-04 17:16:26 +0530

    webpack css

--- REACT_TASKS/webpack-for-react/webpack.config.js
+++ REACT_TASKS/webpack-for-react/webpack.config.js
@@ -1,12 +1,14 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 module.exports = {
-  mode: 'development',  
+  mode: 'development',
   entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, './public'),
     filename: 'bundle.[hash].js'
   },
   devtool: 'inline-source-map',
@@ -20,31 +22,29 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localsConvention: 'camelCase',
-              sourceMap: true
-            }
-          }
+          'style-loader',
+          'css-loader'
         ]
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './public'),
+    },
+    hot: true,
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
       favicon: 'public/favicon.ico'
     })
-  ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    historyApiFallback: true,
-    open: true
-  }
+  ]
 };
