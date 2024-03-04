8b93338eec1f99623c7a7091aca0946cd52d407c
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-03-04 18:19:38 +0530

    HMR

--- REACT_TASKS/webpack-for-react/webpack.config.js
+++ REACT_TASKS/webpack-for-react/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
@@ -28,15 +28,14 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        use: {
-          loader: "url-loader",
-        },
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
         use: {
           loader: "url-loader",
+          options: {
+            limit: 8192, // Convert files < 8kb to base64 strings
+            name: "[name].[hash].[ext]",
+            outputPath: "assets",
+          },
         },
       },
     ],
